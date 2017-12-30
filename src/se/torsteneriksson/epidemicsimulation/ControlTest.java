7d52580e83342da81589053b8b624a980bbf4f60
ermteri <ermteri@gmail.com>
2017-12-31 00:21:18 +0100

    Added test cases in ControlTest

--- src/se/torsteneriksson/epidemicsimulation/ControlTest.java
+++ src/se/torsteneriksson/epidemicsimulation/ControlTest.java
@@ -6,16 +6,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControlTest {
-
+    private Control ctl;
     @BeforeEach
     void setUp() {
+        String args[] = {"-cp", "0", "-dmi", "2", "-dma", "10", "-dp", "10", "-ps", "10"};
+        ctl = new Control(new UserInput(args));
     }
 
     @Test
     void init() {
+        ctl.init();
     }
 
     @Test
     void startSimulation() {
+        ctl.init();
+        ctl.startSimulation();
     }
 }
