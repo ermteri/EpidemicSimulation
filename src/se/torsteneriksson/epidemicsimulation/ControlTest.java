2e5479ac6bba8f8b2b93b3084e5235181b744113
ermteri <ermteri@gmail.com>
2017-12-31 00:15:41 +0100

    Added JUnit for Control class

---
+++ src/se/torsteneriksson/epidemicsimulation/ControlTest.java
@@ -0,0 +1,21 @@
+package se.torsteneriksson.epidemicsimulation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControlTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void startSimulation() {
+    }
+}
