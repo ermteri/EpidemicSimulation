3974b4f9478d6af185fd85492705d1f6c3ab90c6
ermteri <ermteri@gmail.com>
2018-01-02 09:33:18 +0100

    Added test cases in ControlTest

---
+++ src/se/torsteneriksson/epidemicsimulation/Population.java
@@ -0,0 +1,106 @@
+package se.torsteneriksson.epidemicsimulation;
+
+import java.util.Iterator;
+
+public class Population implements Iterable {
+    private HumanBeing[][] mPopulation;
+    private Utilities mUtil;
+    private int curr_x = 0;
+    private int curr_y = 0;
+
+    public Population(HumanBeing[][] hb) {
+        mPopulation = hb;
+        for(int x = 0; x < hb.length;x++) {
+            for (int y = 0; y < hb[x].length ; y++) {
+                mPopulation[x][y] = new HumanBeing();
+            }
+        }
+        mPopulation[hb.length/2][hb.length/2].setHealthState(HumanBeing.Healthstate.SICK);
+        mUtil = new Utilities();
+
+    }
+    @Override
+    public Iterator iterator() {
+        curr_x = 0;
+        curr_y = 0;
+        Iterator it = new Iterator() {
+            private int x = 0;
+            private int y = 0;
+
+            @Override
+            public boolean hasNext() {
+                return x < mPopulation.length && y < mPopulation[x].length;
+            }
+
+            private void resetIndexes() {
+                if (y == mPopulation[x].length) {
+                    y = 0;
+                    x++;
+                }
+            }
+
+            @Override
+            public HumanBeing next() {
+                if (!hasNext()) {
+                    return null;
+                }
+                while (x < mPopulation.length) {
+                    while (y < mPopulation[x].length) {
+                        HumanBeing hb = mPopulation[x][y];
+                        curr_x = x;
+                        curr_y = y;
+                        y++;
+                        resetIndexes();
+                        return hb;
+                    }
+                    x++;
+                }
+                return null;
+            }
+
+            @Override
+            public void remove() {
+                // do nothing
+            }
+        };
+        return it;
+    }// End method iterator
+
+    public int getSize() {
+        return mPopulation.length*mPopulation.length;
+    }
+
+    public boolean checkIfBeInfected(int probability) {
+        if(!mPopulation[curr_x][curr_y].isImune()) {
+            if(isNeighbourInfected(curr_x,curr_y)) {
+                if(mUtil.isHappening(probability)) {
+                    mPopulation[curr_x][curr_y].setHealthState(HumanBeing.Healthstate.SICK);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean isNeighbourInfected(int x, int y) {
+        for(int X = x-1;X <= x +1;X++)
+            for(int Y = y-1;Y <= y+1;  Y++) {
+                if(isValidMatrixPos(X,Y) && x != X && y != Y) {
+                    if((mPopulation[X][Y].getHealthState() == HumanBeing.Healthstate.SICK) &&
+                            mPopulation[X][Y].getSickDays() > 0 )
+                        return true;
+                }
+            }
+        return false;
+    }
+
+    private boolean isValidMatrixPos(int x, int y) {
+        if((x >= 0) &&
+                (x < mPopulation.length)&&
+                (y >= 0) &&
+                (y < mPopulation[x].length))
+            return true;
+        else
+            return false;
+    }
+}
